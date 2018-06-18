c74ea035651cc0d43f09acf6a96a4dfb099fe2ad
Frac7 <francescacella96@gmail.com>
2018-06-18 23:36:41 +0200

    Correzione metodo lista camere, implementazione metodi restanti __unicode__

--- GestioneHotel/models.py
+++ GestioneHotel/models.py
@@ -1,4 +1,4 @@
-#TODO: setters
+#TODO: getters
 from django.db import models
 import django
 from django.utils import timezone
@@ -16,9 +16,15 @@ class Albergatore(models.Model):
     def prenotazioniPerAlbergatore(self):
         pass
 
+    def __unicode__(self):
+        return "%s, %s, email: %s " % (self.nome, self.cognome, self.email)
+
 class Indirizzo(models.Model):
     via = models.CharField(max_length=50,default="")
     numero = models.CharField(max_length=5,default="")
+
+    def __unicode__(self):
+        return "%s, %s " % (self.via, self.numero)
 
 
 class Servizio(models.Model):
@@ -48,6 +54,9 @@ class Hotel(models.Model):
         return Camera.objects.filter(hotel=self)
     #TODO: aggiungi camera
 
+    def __unicode__(self):
+        return "%s, %s " % (self.nome, self.citta)
+
 class Camera(models.Model):
     numero = models.CharField(max_length=15,default="")
     postiLetto = models.IntegerField(default=1)
@@ -57,16 +66,25 @@ class Camera(models.Model):
         pass
     def listaServizi(self): #restituisce la lista di servizi per la camera
         serviziPerCamera = []
-        for servizio in ServiziDisponibili.objects.filter(camera=self.id):
-            serviziPerCamera.append(Servizio.objects.filter(id=servizio.id))
+        for servizioDisponibile in ServiziDisponibili.objects.filter(camera=self.id):
+            serviziPerCamera.append(Servizio.objects.filter(id=servizioDisponibile.servizio.id))
         return serviziPerCamera
+
+    def __unicode__(self):
+        return "numero: %s, %s " % (self.numero, self.hotel)
 
 class ServiziDisponibili(models.Model):
     camera=models.ForeignKey(Camera)
     servizio=models.ForeignKey(Servizio)
+
+    def __unicode__(self):
+        return "%s: %s " % (self.camera, self.servizio)
 
 class Prenotazione(models.Model):
     camera=models.ForeignKey(Camera)
     utente=models.CharField(max_length=50,default="")
     checkin=models.DateField(default=django.utils.timezone.now)
     checkout=models.DateField(default=django.utils.timezone.now)
+
+    def __unicode__(self):
+        return "%s: %s " % (self.camera, self.utente)
