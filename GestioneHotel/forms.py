c6597976fb21f66a9424b239211bf3c3d71fc120
DzhumanB <bohdan1995@live.it>
2018-06-20 21:24:51 +0200

    *REVERT dei file persi in Style.css e form.py.

--- GestioneHotel/forms.py
+++ GestioneHotel/forms.py
@@ -5,20 +5,20 @@ from GestioneHotel.models import *
 class AggiungiCameraForm(forms.Form):
     numero = forms.CharField(label="Numero",max_length=15,required=True,widget=forms.TextInput(attrs={'id': 'campoCamera'}))
     elencoPosti = [(i, i) for i in range(1, 7)]
-    postiLetto = forms.ChoiceField(label="Posti Letto", choices=elencoPosti)
+    postiLetto = forms.ChoiceField(label="Posti Letto", choices=elencoPosti, widget={'id' : 'postiCamera'})
     servizio1 = forms.BooleanField(required=False,label="TV",widget=forms.CheckboxInput(attrs={'id': 'servizioCamera'}))
     servizio2 = forms.BooleanField(required=False,label="Aria condizionata",widget=forms.CheckboxInput(attrs={'id': 'servizioCamera'}))
     servizio3 = forms.BooleanField(required=False,label="Frigo bar",widget=forms.CheckboxInput(attrs={'id': 'servizioCamera'}))
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(label="Email",required=True)
-    password = forms.CharField(label="Password",required=True,widget=forms.PasswordInput)
+    email = forms.EmailField(label="Email",required=True, widget=forms.TextInput(attrs={'id' : 'accediLogin'}))
+    password = forms.CharField(label="Password",required=True,widget=forms.PasswordInput(attrs={'id' : 'accediPassword'}))
 
 class RegistrazioneAlbergatore(forms.Form):
-    nome = forms.CharField(label="nome", max_length=50,required=True)
-    cognome = forms.CharField(label="cognome", max_length=50,required=True)
-    email = forms.EmailField(label="email", max_length=50,required=True)
-    password = forms.CharField(label="password", max_length=32,required=True)
+    nome = forms.CharField(label="nome", max_length=50,required=True, widget=forms.TextInput(attrs={'id':'registrazioneNome'}))
+    cognome = forms.CharField(label="cognome", max_length=50,required=True, widget=forms.TextInput(attrs={'id':'registrazioneCognome'}))
+    email = forms.EmailField(label="email", max_length=50,required=True, widget=forms.TextInput(attrs={'id':'registrazioneEmail'}))
+    password = forms.CharField(label="password", max_length=32,required=True, widget=forms.PasswordInput(attrs={'id':'registrazionePassword'}))
 
 class AggiungiHotelForm(forms.Form):
      nome = forms.CharField(label="Nome", max_length=30, required=True,
@@ -37,17 +37,17 @@ class FormRicerca(forms.Form):
     #recupero l'elenco delle citta dagli hotel
     elencoCitta = [(hotel.citta, hotel.citta) for hotel in Hotel.objects.order_by('citta')]
     # html select della citta dell'elenco delle citta disponibili
-    citta=forms.ChoiceField(label="Destinazione",widget=forms.Select, choices=elencoCitta)
+    citta=forms.ChoiceField(label="Destinazione", widget=forms.Select(attrs={'id':'ricercaCitta'}), choices=elencoCitta)
     # input data arrivo
-    dataArrivo = forms.DateField(label="Arrivo",widget=forms.SelectDateWidget())
+    dataArrivo = forms.DateField(label="Arrivo",widget=forms.SelectDateWidget(attrs={'id':'ricercaDataIn'}))
     #input data partenza
-    dataPartenza = forms.DateField(label="Partenza",widget=forms.SelectDateWidget())
+    dataPartenza = forms.DateField(label="Partenza",widget=forms.SelectDateWidget(attrs={'id':'ricercaDataOut'}))
     elencoPosti=[(i,i) for i in range(1, 7)]
-    posti = forms.ChoiceField(label="Posti",choices=elencoPosti)
+    posti = forms.ChoiceField(label="Posti",choices=elencoPosti, widget=forms.Select(attrs={'id':'ricercaPostiLetto'}))
 
 class FormPrenota(forms.Form):
     # input dell'indirizzo Email dell'utente che effettua la prenotazione
-    prenotaUtente = forms.EmailField(label="Email")
-    prenotaCheckin = forms.CharField(widget=forms.HiddenInput())
-    prenotaCheckout = forms.CharField(widget=forms.HiddenInput())
-    prenotaIdCamera = forms.CharField(widget=forms.HiddenInput())
+    prenotaUtente = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'id':'prenotaUtente'}))
+    prenotaCheckin = forms.CharField(widget=forms.HiddenInput(attrs={'id':'prenotaDataIn'}))
+    prenotaCheckout = forms.CharField(widget=forms.HiddenInput(attrs={'id':'prenotaDataOut'}))
+    prenotaIdCamera = forms.CharField(widget=forms.HiddenInput(attrs={'id':'prenotaIdCamera'}))
